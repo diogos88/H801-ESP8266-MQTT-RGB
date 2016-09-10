439876b47ce6b68271d660b237d902e2782a47e5
Diogo Soares <info@diogosoares.ca>
2016-09-10 00:13:12 -0400

    Added secrets.h

--- src/secrets.h
+++ src/secrets.h
@@ -1,14 +1,14 @@
 #pragma once
 
-const char *ssid = "No Internet Access";
-const char *password = "^-/<8++<0L/g>cB";
+const char *ssid = "Network";
+const char *password = "p4ssw0rd";
 
-const char* mqtt_server = "192.168.2.24";
+const char* mqtt_server = "127.0.0.1";
 const int mqtt_port = 1883;
-const char* mqtt_user = "pi";
-const char* mqtt_password = "raspberry";
-char* mqtt_command_topic = (char *)"HA/LEDController001/";
-char* mqtt_status_topic = (char *)"HA/LEDController001/Status/";
+const char* mqtt_user = "user";
+const char* mqtt_password = "p4ssw0rd";
+char* mqtt_command_topic = (char *)"inTopic/";
+char* mqtt_status_topic = (char *)"outTopic/";
 char* mqtt_rgb_topic = (char *)"rgb/";
 char* mqtt_w1_topic = (char *)"w1/";
 char* mqtt_w2_topic = (char *)"w2/";
